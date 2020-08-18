19f3cef06d37397b5f14e2aa24e83ac4e02b3633
Ron Huberfeld <32178925+ron-huberfeld@users.noreply.github.com>
2020-08-18 09:48:00 +0300

    First draft - script to load data to list

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
+async function fetchText() {
+  let response = await fetch("./abba/songs.txt");
+  console.log(response.status); // 200
+  console.log(response.statusText); // OK
+  if (response.status === 200) {
+    let data = await response.text();
+    console.log(data);
+    // handle data
+  }
+}
 
-//here you write all the code, good luck!
+fetch("https://ghibliapi.herokuapp.com/films")
+  .then((response) => {
+    //JSON data feed URL
+    return response.json();
+  })
+  .then((data) => {
+    // Add songs
+    let html = "";
+    data.map((item) => {
+      let htmlSegment = `<a href="${item.title}.mp3">${item.title}</a>`;
+      html += htmlSegment;
+    });
+    const songItem = document.querySelector(".songs-list");
+    songItem.innerHTML = html;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+/* Root Element */
+const app = document.getElementById("my_player");
+
+/* Songs list */
+const songs = document.createElement("div");
+songs.setAttribute("class", "songs-list");
+app.appendChild(songs);
+
+// fetchText();
